ab91c676ef75f079b6b6e089c9e132949d8020ac
unknown <adshah16@asu.edu>
2025-04-10 15:36:42 +0530

    test-ticker

---
+++ packages/app-sundevil/src/components/HeaderTicker/ticker.stories.js
@@ -0,0 +1,62 @@
+// @ts-check
+import React from "react";
+
+import { Ads } from "../Ads";
+import HeaderTicker from "./headerTicker";
+
+/** @type {import("../Ads").Props} */
+const props = {
+  // width: 970,
+  // height: 66,
+  storage: window.localStorage,
+  storageKey: "sun-devils-ads",
+  id: "root-ad",
+  target: "_blank",
+  ads: [
+    {
+      imageSrc: "https://tpc.googlesyndication.com/simgad/15906690503191566407",
+      imageAlt: "Placeholder",
+      href: "https://www.example.com",
+      id: "some-ad",
+      target: "_blank",
+      // width: 970,
+      // height: 66,
+    },
+    {
+      imageSrc: "https://tpc.googlesyndication.com/simgad/4947683613591479131",
+      imageAlt: "Placeholder",
+      href: "https://www.example.com",
+      target: "_blank",
+      // width: 970,
+      // height: 66,
+    },
+    {
+      imageSrc: "https://tpc.googlesyndication.com/simgad/8450655694590921544",
+      imageAlt: "Placeholder",
+      href: "https://www.example.com",
+
+      target: "_blank",
+      // width: 728,
+      // height: 90,
+    },
+  ],
+};
+
+export default {
+  title: "Test Ticker",
+  component: args => <HeaderTicker />,
+  parameters: {
+    docs: {
+      description: {
+        component: " ",
+      },
+    },
+  },
+};
+
+const Template = args => {
+  return <HeaderTicker />;
+};
+
+export const Default = Template.bind({});
+Default.args = props;
