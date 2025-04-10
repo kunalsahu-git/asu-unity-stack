e573c2b029ec38a243387a7051b1db37e1c01715
unknown <adshah16@asu.edu>
2025-04-10 11:56:54 +0530

    removing variant static

--- packages/app-sundevil/src/components/SectionHeader/SponsorBlock/Variant.js
+++ packages/app-sundevil/src/components/SectionHeader/SponsorBlock/Variant.js
@@ -1,50 +1,17 @@
 /* eslint-disable react/prop-types */
 // @ts-check
 import React, { forwardRef } from "react";
-
-import { isSponsorBlockStatic, isSponsorGoogleAd } from "./props";
 import { VariantGoogleAd } from "./VariantGoogleAd";
-import { VariantStatic } from "./VariantStatic";
 
 /**
  * @type {React.FC<import("../props").SectionHeaderProps >}
  */
 export const Variant = forwardRef((props, ref) => {
-  const { sponsorBlock } = props;
-  // if (isSponsorGoogleAd(sponsorBlock)) {
     return (
-      <>
         <VariantGoogleAd
         {...props}
         // @ts-ignore
         ref={ref}
       />
-      <p>hello</p>
-
-      <VariantStatic
-        {...props}
-        // @ts-ignore
-        ref={ref}
-      />
-      </>
     );
-  // }
-
-  if (isSponsorBlockStatic(sponsorBlock)) {
-    return (
-      <VariantStatic
-        {...props}
-        // @ts-ignore
-        ref={ref}
-      />
-    );
-  }
-
-  return (
-    <VariantStatic
-      {...props}
-      // @ts-ignore
-      ref={ref}
-    />
-  );
 });
