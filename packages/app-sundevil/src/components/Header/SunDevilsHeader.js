a1269ee691dba6c1a8356c6df14b5f86614489f5
unknown <adshah16@asu.edu>
2025-05-09 18:26:08 +0530

    using props for ticker

--- packages/app-sundevil/src/components/Header/SunDevilsHeader.js
+++ packages/app-sundevil/src/components/Header/SunDevilsHeader.js
@@ -2,18 +2,18 @@
 import React from "react";
 
 import { ASUHeader } from "../../../../component-header/src";
-import { TickerCarousel } from "../TestTickerCarousel";
+// import { TickerCarousel } from "../TestTickerCarousel";
 import { headerPropTypes } from "./props";
 import { mapProps } from "./props-map";
 
-const tickerProps = {
-  tickerAPI: "https://hokiesports.com/website-api/schedule-events?filter%5Bpast%5D=true&sort%5B0%5D=datetime&include%5B0%5D=conference.image&include%5B1%5D=opponent.customLogo&include%5B2%5D=opponent.officialLogo&include%5B3%5D=opponentLogo&include%5B4%5D=postEventArticle.image&include%5B5%5D=preEventArticle.image&include%5B6%5D=presentedBy&include%5B7%5D=schedule.sport&include%5B8%5D=scheduleEventLinks.icon&include%5B9%5D=scheduleEventResult&include%5B10%5D=secondOpponent.customLogo&include%5B11%5D=secondOpponent.officialLogo&include%5B12%5D=secondOpponentLogo&include%5B13%5D=tournament&per_page=100&page=1"
-}
+// const tickerProps = {
+//   tickerAPI: "https://hokiesports.com/website-api/schedule-events?filter%5Bpast%5D=true&sort%5B0%5D=datetime&include%5B0%5D=conference.image&include%5B1%5D=opponent.customLogo&include%5B2%5D=opponent.officialLogo&include%5B3%5D=opponentLogo&include%5B4%5D=postEventArticle.image&include%5B5%5D=preEventArticle.image&include%5B6%5D=presentedBy&include%5B7%5D=schedule.sport&include%5B8%5D=scheduleEventLinks.icon&include%5B9%5D=scheduleEventResult&include%5B10%5D=secondOpponent.customLogo&include%5B11%5D=secondOpponent.officialLogo&include%5B12%5D=secondOpponentLogo&include%5B13%5D=tournament&per_page=100&page=1"
+// }
 export const SunDevilsHeader = props => {
   const mappedProps = { ...mapProps(props) };
   return <>
    <ASUHeader {...mappedProps} />
-   <TickerCarousel {...tickerProps}/>
+   {/* <TickerCarousel /> */}
   </>
  ;
 };
