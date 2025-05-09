3334b111234547eb86254a4aef04a8a84d775862
unknown <adshah16@asu.edu>
2025-05-09 15:57:49 +0530

    adding index for ticker

--- packages/app-sundevil/src/components/Header/SunDevilsHeader.js
+++ packages/app-sundevil/src/components/Header/SunDevilsHeader.js
@@ -2,12 +2,16 @@
 import React from "react";
 
 import { ASUHeader } from "../../../../component-header/src";
-// import { TickerCarousel } from "../../../src/components/TestTickerCarousel"
+// import { TickerCarousel } from "../TestTickerCarousel";
 import { headerPropTypes } from "./props";
 import { mapProps } from "./props-map";
 
 export const SunDevilsHeader = props => {
   const mappedProps = { ...mapProps(props) };
-  return <ASUHeader {...mappedProps} />;
+  return <>
+   <ASUHeader {...mappedProps} />
+   {/* <TickerCarousel /> */}
+  </>
+ ;
 };
 SunDevilsHeader.propTypes = headerPropTypes;
