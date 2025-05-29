2af309e645a58c9d05a4eda42e285d0d1c7d6f16
akhanam <akhanam@asu.edu>
2025-05-29 15:50:28 +0530

    Single video section with background

---
+++ packages/app-sundevil/src/components/SingleVideoSection/SingleVideoSection.js
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import { VideoSection } from "../VideoSection";
+
+import "./style.css";
+import { repeat } from "../../utils/repeat";
+
+const propTypes = {
+  title: PropTypes.string,
+  body: PropTypes.string,
+  bgdesktopImage: PropTypes.string,
+  bgmobileImage: PropTypes.string,
+  videoData: PropTypes.object,
+};
+
+export const SingleVideoSection = ({
+  title,
+  body,
+  bgdesktopImage,
+  bgmobileImage,
+  videoData,
+}) => {
+  return (
+    <section>
+      <div
+        className="pt-8 d-lg-block d-md-block d-none pb-12 sda-single-video-section desktop-image"
+        style={{
+          backgroundImage: `url(${bgdesktopImage})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="text-center mx-auto section-content">
+          <div className="container">
+            <h2 className="mb-4 text-center">{title}</h2>
+            <p className="mb-lg-10 mb-md-10 mb-6">{body}</p>
+          </div>
+          <VideoSection {...videoData} className="video-section" />
+        </div>
+      </div>
+      <div className="pt-8 d-lg-none d-md-none d-block pb-12 sda-single-video-section">
+        <div className="text-center mx-auto section-content">
+          <div className="container">
+            <h2 className="mb-4 text-center">{title}</h2>
+            <p className="mb-lg-10 mb-md-10 mb-6">{body}</p>
+          </div>
+          <div
+            className="pt-6 pb-8 mobile-image"
+            style={{
+              backgroundImage: `url(${bgmobileImage})`,
+              backgroundSize: "cover",
+            }}
+          >
+            <VideoSection {...videoData} className="video-section" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+SingleVideoSection.propTypes = propTypes;
+
+export default SingleVideoSection;
