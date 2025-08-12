e49bbaf33a5ade2816ecbb6918cd0f047cfd236d
akhanam <akhanam@asu.edu>
2025-08-12 23:36:24 +0530

    ticker reverted

--- packages/app-sundevil/src/components/TickerSection/TickerSection.js
+++ packages/app-sundevil/src/components/TickerSection/TickerSection.js
@@ -6,7 +6,7 @@ import { SportIcon } from "../../../../app-sundevil/src/components/SportIcon";
 import { stringToClosestSportName } from "../../../../app-sundevil/src/components/SportIcon/sport-name";
 import { GameDataTicker } from "./game-data-ticker";
 
-export const TickerCarousel = ({ tickerAPI }) => {
+export const TickerSection = ({ tickerAPI }) => {
   const [items, setItems] = useState([]);
   const [position, setPosition] = useState(0);
   const [nextLink, setNextLink] = useState(null);
@@ -18,10 +18,10 @@ export const TickerCarousel = ({ tickerAPI }) => {
 
   const slideLeft = () => setPosition(prev => Math.min(prev + itemWidth, 0));
 
-  const winningHighlightStyle = (highlight) => ({
-    background: highlight ? '#FFC627' : '#191919',
-    color: highlight ? '#191919' : '#D0D0D0',
-    padding: '2px',
+  const winningHighlightStyle = highlight => ({
+    background: highlight ? "#FFC627" : "#191919",
+    color: highlight ? "#191919" : "#D0D0D0",
+    padding: "2px",
   });
 
   const fetchData = async (url = tickerAPI) => {
@@ -30,7 +30,9 @@ export const TickerCarousel = ({ tickerAPI }) => {
     try {
       const dataSource = new GameDataTicker(url);
       const data = await dataSource.findMany();
-      const games = data.games.sort((a, b) => new Date(b.gameday) - new Date(a.gameday));
+      const games = data.games.sort(
+        (a, b) => new Date(b.gameday) - new Date(a.gameday)
+      );
       setItems(prev => [...prev, ...games]);
       setNextLink(data.nextLink);
     } catch (e) {
@@ -65,12 +67,16 @@ export const TickerCarousel = ({ tickerAPI }) => {
           {items.map((item, index) => (
             <div key={index} className="carousel-item">
               <div className="line">
-                <div style={{ color: '#fafafa' }}>
-                  <SportIcon sportName={stringToClosestSportName(item.sportName)} />
+                <div style={{ color: "#fafafa" }}>
+                  <SportIcon
+                    sportName={stringToClosestSportName(item.sportName)}
+                  />
                 </div>
                 {item.sportName}
               </div>
-              <div className="line" style={{ fontWeight: "normal" }}>{item.gameday}</div>
+              <div className="line" style={{ fontWeight: "normal" }}>
+                {item.gameday}
+              </div>
               <div className="line">
                 <div style={winningHighlightStyle(item.firstTeam.won)}>
                   {item.firstTeam.name} {item.firstTeam.score}
@@ -96,4 +102,4 @@ export const TickerCarousel = ({ tickerAPI }) => {
   );
 };
 
-export default TickerCarousel;
+export default TickerSection;
